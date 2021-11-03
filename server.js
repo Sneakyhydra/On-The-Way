5820b856cc968ce28844d6a45027f74a0d38bab5
Dhruv Rishishwar <63316728+Sneakyhydra@users.noreply.github.com>
2021-11-03 22:47:50 +0530

    Production build

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express"); // Create server
 const cors = require("cors"); // Cors middleware
 const cookieParser = require("cookie-parser"); // Cookies
+const path = require('path');
 
 // Init app
 const app = express();
@@ -21,6 +22,14 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/admin', require('./routes/admin'));
 app.use('/api/counsellor', require('./routes/counsellor'));
 app.use('/api/student', require('./routes/student'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 // Listen to port
 app.listen(port, () => console.log(`Listening on port ${port}`));

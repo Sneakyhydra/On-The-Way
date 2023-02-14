6c643c2c07433ed6b8387204057453611b21ea60
Sneakyhydram <dhruvrishishwar2@gmail.com>
2023-02-14 22:04:58 +0530

    changed db

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ const mysql = require("mysql2"); // Connect to the DB
 
 // Create the pool
 const pool = mysql.createPool({
-    host: "remotemysql.com",
-    user: "PCige3566j",
-    password: "0rgo2Zpkd4",
-    database: "PCige3566j",
+    host: "db4free.net",
+    port: 3306,
+    user: "sneakyhydra",
+    password: "sneakyhydra",
+    database: "studentcoun",
     waitForConnections: true,
     connectionLimit: 2,
     queueLimit: 0

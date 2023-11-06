c28a1e23fbd56ae4a6c866498c5b6437dfc97fd9
Sneakyhydra <dhruvrishishwar2@gmail.com>
2023-11-06 23:14:42 +0530

    renovation 1

--- DATABASE/db.js
+++ DATABASE/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2"); // Connect to the DB
 // Create the pool
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,

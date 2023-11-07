44aff1b0acc3c99cb1a0295f0d0e7f4658e28087
Sneakyhydra <dhruvrishishwar2@gmail.com>
2023-11-07 21:11:39 +0530

    renovation frontend

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,2 +1,6 @@
-export const API_URL = 'https://on-the-way-api.vercel.app/api';
+// export const API_URL = 'https://on-the-way-api.vercel.app/api';
 // export const API_URL = 'http://localhost:5000/api';
+export const API_URL =
+	process.env.NODE_ENV === 'development'
+		? process.env.REACT_APP_DEV_URL
+		: process.env.REACT_APP_PRO_URL;
